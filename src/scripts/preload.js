3b73ba9abb6fedffd7369e8aec002c5e81c1c88d
Alex Devero <deveroalex@gmail.com>
2021-01-04 16:09:15 +0100

    Add preload script

---
+++ src/scripts/preload.js
@@ -0,0 +1,8 @@
+// All of the Node.js APIs are available in the preload process.
+// It has the same sandbox as a Chrome extension.
+
+// const foo = require('foo')
+
+// window.addEventListener('DOMContentLoaded', () => {
+//   window.foo = foo
+// })
